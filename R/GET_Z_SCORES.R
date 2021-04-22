44bee84fb13be3b01c84e30d4471566dd642f725
eddiehli <hongyu.li@yale.edu>
2021-04-22 14:13:05 -0400

    update

--- R/GET_Z_SCORES.R
+++ R/GET_Z_SCORES.R
@@ -11,14 +11,10 @@
 get_z_scores = function(data) {
   
   expression_mat = data$expression_mat
-  n_group = data$n_group
-  n_sample = data$n_sample
-  n_gene = data$n_gene
-  n_cell = data$n_cell
-  n_de_gene = data$n_de_gene
-  n_de_cell = data$n_de_cell
-  g_g = data$g_g
-  c_c = data$c_c
+  gene_gene = data$gene_gene
+  cell_cell = data$cell_cell
+  n_gene = dim(gene_gene)[1]
+  n_cell = dim(cell_cell)[1]
   
   zz = array(NA, dim = c(1, n_gene, n_cell))
   for (g in 1:n_gene) for (c in 1:n_cell) zz[1,g,c] = qnorm(pt(t.test(expression_mat[1,,g,c], expression_mat[2,,g,c])$statistic,
